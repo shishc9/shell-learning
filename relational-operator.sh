c38d73ac54643b35ddb27f4a16f9dbdc21ba79ef
shishc9 <343141457@qq.com>
2022-06-11 23:48:17 +0800

    operator

--- relational-operator.sh
+++ relational-operator.sh
@@ -48,3 +48,17 @@ fi
 -o	或运算，有一个表达式为 true 则返回 true。	[ $a -lt 20 -o $b -gt 100 ] 返回 true。
 -a	与运算，两个表达式都为 true 才返回 true。	[ $a -lt 20 -a $b -gt 100 ] 返回 false。
 eof
+
+:<<eof
+&&	逻辑的 AND	[[ $a -lt 100 && $b -gt 100 ]] 返回 false
+||	逻辑的 OR	[[ $a -lt 100 || $b -gt 100 ]] 返回 true
+eof
+
+:<<eof
+=	检测两个字符串是否相等，相等返回 true。	[ $a = $b ] 返回 false。
+!=	检测两个字符串是否不相等，不相等返回 true。	[ $a != $b ] 返回 true。
+-z	检测字符串长度是否为0，为0返回 true。	[ -z $a ] 返回 false。
+-n	检测字符串长度是否不为 0，不为 0 返回 true。	[ -n "$a" ] 返回 true。
+$	检测字符串是否不为空，不为空返回 true。	[ $a ] 返回 true。
+eof
+
